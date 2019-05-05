08d4667ef7645d160f8a6b346a6ec39107c08bdf
ErikT <erik.trickel@asu.edu>
2019-05-05 09:49:45 -0700

    Exploit now works without docker flag, changed method of creating seccomp rules, but with same result only sigreturn, exit, write, and read are enabled. libseccomp-dev was added service container.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 [![Build Status](https://travis-ci.com/o-o-overflow/dc2019q-speedrun-011.svg?token=6XM5nywRvLrMFwxAsXj3&branch=master)](https://travis-ci.com/o-o-overflow/dc2019q-speedrun-011)
 
 Blind shellcoding (shellcode where the flag is in memory but there is no output, all fds are closed and seccomp is in place).
+
+#Compiling service 
+
+Service compilation now requires -lseccomp, use the following. 
+
+`gcc service/src/speedrun-011.c -lseccomp -o service/speedrun-011`
+
+
--- service/src/speedrun-011.c
+++ service/src/speedrun-011.c
@@ -13,6 +13,8 @@
 #include <sys/ptrace.h>
 #include <fcntl.h>
 
+#include <seccomp.h> /* libseccomp */
+
 void say_hello()
 {
    printf("Can you drive with a blindfold?\n");
@@ -39,23 +41,53 @@ char* get_flag()
 
 void shellcode_it(char* buf, unsigned int size)
 {
-
+   int rc = 0;
+   scmp_filter_ctx ctx;
    char* flag = get_flag();
 
    void *ptr = mmap(0, size, PROT_EXEC | PROT_WRITE | PROT_READ, MAP_ANON | MAP_PRIVATE, -1, 0);
    void (*shell)(char*);
-   memcpy(ptr, buf, size);
 
+   memcpy(ptr, buf, size);
 
    // Close all possible communication channels
    close(0);
    close(1);
    close(2);
 
+
+   ctx = seccomp_init(SCMP_ACT_KILL); // default action: kill
+
+   // setup strict whitelist
+   rc += seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(rt_sigreturn), 0);
+   rc += seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit), 0);
+   rc += seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(read), 0);
+   rc += seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 0);
+
+   if (rc != 0) {
+        perror("seccomp_rule_add failed");
+        exit(-2);
+   }
+
+   // load the filter
+   seccomp_load(ctx);
+   if (rc != 0) {
+        perror("seccomp_load failed");
+        exit(-1);
+   }
+
+   if (getenv("TEST_SECCOMP") != NULL) {
+      int filedesc = open("/dev/random", O_RDONLY );
+
+   }
+
+/*
    if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_STRICT) != 0)
    {
 	  exit(-1);
    }
+*/
+
 
    shell = (void (*)(char*)) ptr;
    shell(flag);
